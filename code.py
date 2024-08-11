ea6d85fbd9c887e000824982ab6e768809c75012
Rutuja Pawale <160813855+RutujaPawale@users.noreply.github.com>
2024-08-11 21:27:54 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,1 +1,8 @@
-
+def bubbleSort():
+    print("Before sorting:\n",percentage)
+    for i in range(num-1,0,-1):
+        for j in range(i):
+            if percentage[j]>percentage[j+1]:
+                percentage[j],percentage[j+1]=percentage[j+1],percentage[j]
+    print("After sorting:\n",percentage)
+    topFiveMarks()
